445ca236d4fdd99fdbcb4ed2310f00ab428433d6
Mrbabet <wieliczkokamil@gmail.com>
2024-02-05 20:31:49 +0100

    added redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import Phonebook from "./components/Phonebook/Phonebook";
+import { configureStore } from "redux";
+import { devToolsEnhancer } from "@redux-devtools/extension";
 
 const App = () => {
   return (
